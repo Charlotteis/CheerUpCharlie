0435ee105e56a362bed068ff5a377ab85550c748
Charlotte Spencer <charlottelaspencer@gmail.com>
2013-10-27 19:23:41 +0000

    Turned off text sending.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,14 +4,14 @@ $('form button').on('click', function(e) {
 
     // Based on the selected demo, fire off an ajax request
     // We expect just a string of text back from the server (keeping it simple)
-    var url = '/message';
+    /* var url = '/message';
     $.ajax(url, {
         method:'POST',
         dataType:'text',
         data:{
             body:$('#body').val()
         },
-    });
+    }); */
 
     $("#image").attr('src',"images/happycharlie.png");
     $("#button").html("Thanks!");
